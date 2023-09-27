50a93daa9469439455ec269e9e92ae3f063f595e
Frikhol <dn_den@mail.ru>
2023-09-27 13:14:35 +0300

    db class

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 
 public class App {
     public static void main(String[] args) throws URISyntaxException {
+
         Api.getUpdates();
         Api.printChatIds();
         Api.sendMessage(123456,"I am still alive");
