900bbc5e0da127e280f729ee1ab11ce7a04d0a16
aliamanticarpa <aliamanticarpa@mail.ru>
2023-09-27 13:33:16 +0300

    add db

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -1,4 +1,53 @@
 package db;
 
+import java.sql.*;
+
 public class DataBase {
+    public static void main(String[] args) {
+        try {
+            Conn();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+    private static String PATH= "resources/DataBase/db.db";
+
+    public static void setPATH(String PATH) {
+        DataBase.PATH = PATH;
+    }
+
+    private static Connection conn = null;
+    private static ResultSet resultSet;
+    // --------ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ--------
+    public static void Conn() throws ClassNotFoundException, SQLException
+    {
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:"+PATH);
+    }
+    public ResultSet read(String order) throws SQLException {
+        Statement statmt = conn.createStatement();
+        resultSet = statmt.executeQuery(order);
+        statmt.close();
+        return resultSet;
+    }
+
+    public boolean update(String order) throws SQLException {
+        Statement statmt = conn.createStatement();
+        boolean result = statmt.execute(order);
+        statmt.close();
+        return result;
+    }
+
+    public static ResultSet getLastResult() {
+        return resultSet;
+    }
+
+    public static void CloseDB() throws  SQLException
+    {
+        conn.close();
+        resultSet.close();
+        System.out.println("Соединения закрыты");
+    }
 }
